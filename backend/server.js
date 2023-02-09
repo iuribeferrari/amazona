9e2357bb84f7cfef1caa22a2143ebb5aef522a99
iuribeferrari <iuribeferrari@redmagisterial.com>
2023-02-08 21:03:50 -0600

    simboloMas

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import data from './data.js';
 
 const app = express();
-
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//  import data from '../data';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loagin: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.playload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.playload };
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
+  //const [{loading, error, products}, dispatch ] = const [state, dispatch] = useReducer(reducer, {setProducts};
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Productos Destacados </h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
