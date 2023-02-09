01b517b59211b30b7c34ab88792f019fa606c509
iuribeferrari <iuribeferrari@redmagisterial.com>
2023-02-08 20:06:43 -0600

    allune 7

---
+++ backend/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Tortillas Keto',
+      slug: 'tortillas-keto',
+      category: 'salado',
+      image: '/images/p1.png',
+      price: 90,
+      countInStock: 10,
+      brand: 'ViaKeto',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Ricas tortillas Keto con harina Girasol',
+    },
+    {
+      name: 'Totopos Keto',
+      slug: 'totopos-keto',
+      category: 'salado',
+      image: '/images/p2.png',
+      price: 110,
+      countInStock: 15,
+      brand: 'ViaKeto',
+      rating: 5.0,
+      numReviews: 15,
+      description: 'Los mejores totopos para tus chilaquiles sin culpa',
+    },
+    {
+      name: 'Cheese Cake Sugar Free',
+      slug: 'Cheese-cake-sugar-free',
+      category: 'dulce',
+      image: '/images/p3.png',
+      price: 70,
+      countInStock: 13,
+      brand: 'KetoVia',
+      rating: 4.3,
+      numReviews: 13,
+      description:
+        'Delicioso pastellito de queso con mermelada de frutos rojos, todo sin azucar, ni carbo mxz',
+    },
+    {
+      name: 'Dona Keto',
+      slug: 'dona-keto',
+      category: 'dulce',
+      image: '/images/p4.png',
+      price: 80,
+      countInStock: 15,
+      brand: 'KetoVia',
+      rating: 4.8,
+      numReviews: 17,
+      description:
+        'Increibles donas que te harán olvidar que estas a dieta, sin azucar, gluten o carbohidratos',
+    },
+  ],
+};
+
+export default data;
